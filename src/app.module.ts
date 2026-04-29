6b0d15f9d7b8f6e91a862c66befb4e885bf415d6
Abdubakiyev <abdubakiyevabdulloh798@gmail.com>
2026-04-29 16:15:14 +0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { HabitsModule } from './module/habits/habits.module';
 import { UserModule } from './module/user/user.module';
-
+import { AppController } from './app.controller';   // ← qo'shing
+import { AppService } from './app.service';           // ← qo'shing
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     HabitsModule,
-    UserModule
+    UserModule,
   ],
+  controllers: [AppController],   // ← qo'shing
+  providers: [AppService],         // ← qo'shing
 })
 export class AppModule {}
